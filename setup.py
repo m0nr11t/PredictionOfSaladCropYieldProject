c63dc48844caf6258ee57aa4deaeca237f01b791
m0nr11t <maewang405@gmail.com>
2022-09-29 14:53:27 +0700

    with Req file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Multiple_Regression_ML',
+    version='0.0.1',
+    packages=[''],
+    url='',
+    license='',
+    author='m0nr11t',
+    author_email='rattanaprasit@gmail.com',
+    description='Happy Happy Happy'
+)
