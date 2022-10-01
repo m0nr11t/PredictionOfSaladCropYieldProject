22db99bee72d2fef6840bf97547cb2b10d265907
m0nr11t <maewang405@gmail.com>
2022-10-02 01:23:40 +0700

    100222

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Multiple_Regression_ML',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='m0nr11t',
+    author_email='',
+    description=''
+)
